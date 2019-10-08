f09d5a2c3815da231639f5aebf7ee53f2ba16d33
anloffredo <loffredo.ap@gmail.com>
2019-10-07 23:51:37 -0700

    Framework Updates
    Framework Updates

---
+++ DependencyInjection/RepositoryTypeInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Penguin.Persistence.Repositories.DependencyInjection
+{
+    internal class RepositoryTypeInfo
+    {
+        public RepositoryTypeInfo(Type repositoryType, Type objectType)
+        {
+            RepositoryType = repositoryType;
+            ObjectType = objectType;
+        }
+        public Type RepositoryType { get; set; }
+        public Type ObjectType { get; set; }
+    }
+}
